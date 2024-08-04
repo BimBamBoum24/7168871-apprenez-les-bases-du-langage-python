61ec17d4888a745f20b47a6aa581efddb07e01ea
BimBamBoum24 <theo.ariana15@gmail.com>
2024-08-04 10:40:18 +0200

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,13 @@
 # Écrivez votre code ici !
+nom = "Kélian"
+age = 16
+taille = 1,78
+est_etudiant = true
+
+print(f"Je m'appelle {nom}, j'ai {age}, je fais {taille} et je suis étudiant.")
+print(est_etudiant)
+
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
